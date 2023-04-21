88238f35a316000b53fc5183661d51fbba44b14e
Chaithra899 <chaitrasundar831999@gmail.com>
2023-04-21 14:12:59 +0530

    first commit

---
+++ ind.js
@@ -0,0 +1,30 @@
+const hr1=document.getElementById("hour");
+const min=document.getElementById("minutes");
+const sec=document.getElementById("seconds");
+const ampm1=document.getElementById("ampm");
+
+function updateclock()
+{
+    let h= new Date().getHours();
+    let m=new Date().getMinutes();
+    let s=new Date().getSeconds();
+    let ampm ="AM"
+
+    if (h>12)
+    {
+        h=h-12;
+        ampm="PM";
+    }
+    h=h<10?"0"+h:h;
+    m=m<10?"0"+m:m;
+    s=s<10?"0"+s:s;
+
+    hr1.innerText=h;
+    min.innerText=m;
+    sec.innerText=s;
+    ampm1,(innerText=ampm)
+    setTimeout(()=>{
+     updateclock()
+    },1000)
+}
+updateclock()
